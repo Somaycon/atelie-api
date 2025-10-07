cd0b7e8e29b246748ff94e46e6eee816a96f8241
somaycon <contato.somaycon@gmail.com>
2025-10-06 21:21:06 -0300

    refactor: remove inline route handler and delegate to initializeRoutes function

--- router/router.go
+++ router/router.go
@@ -4,10 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func Initalize() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Olá Mundo!",
-		})
-	})
+	initializeRoutes(router)
 	router.Run("8080")
 }
