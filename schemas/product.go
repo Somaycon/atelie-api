c391f460eb28c114aff8bcd070b8f1f84ad4b0a8
somaycon <contato.somaycon@gmail.com>
2025-10-12 10:25:59 -0300

    feat: enhance product schema with response structure

--- schemas/product.go
+++ schemas/product.go
@@ -1,10 +1,24 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Products struct {
 	gorm.Model
 	Name string
 	Price float64
 	Description string
+}
+
+type ProductResponse struct{
+	Id uint `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty"`
+	Name string `json:"name"`
+	Price float64 `json:"price"`
+	Description string `json:"description"`
 }
