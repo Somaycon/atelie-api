5fbed08d5c8f096c2b450bcf12fe65cb56c0f70b
somaycon <contato.somaycon@gmail.com>
2025-10-12 11:36:21 -0300

    feat: implement product creation with validation and error handling

--- handler/createProduct.go
+++ handler/createProduct.go
@@ -1,9 +1,34 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Somaycon/atelie-api/schemas"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateProductHandler(ctx *gin.Context) {
-	ctx.JSON(201, gin.H{
-		"message": "POST de produto!",
-	})
+	request := CreateProductRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	product := schemas.Products{
+		Name:        request.Name,
+		Price:       request.Price,
+		Description: request.Description,
+	}
+
+	if err := db.Create(&product).Error; err != nil {
+		logger.Errorf("Error creating product: %v", err)
+		sendError(ctx, http.StatusInternalServerError,"error creating product on database")
+		ctx.JSON(500, gin.H{"error": "Error creating product"})
+		return
+	}
+	sendSucess(ctx, "create-product", product)
 }
---
+++ handler/response.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, status int, message string) {
+	ctx.Header("Content-type", "aplication/json")
+	ctx.JSON(status, gin.H{
+		"message" :message,
+		"status": status,
+	})
+}
+
+func sendSucess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-type", "aplication/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation %s sucess", op),
+		"data": data,
+	})
+}
