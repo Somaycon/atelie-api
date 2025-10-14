2daa0da672e51402dfcac6c3168cbb66fcdaf4da
somaycon <contato.somaycon@gmail.com>
2025-10-14 11:14:43 -0300

    feat: add Swagger documentation setup and routes

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,20 @@
 package router
 
 import (
+	docs "github.com/Somaycon/atelie-api/docs"
 	"github.com/Somaycon/atelie-api/handler"
 	"github.com/Somaycon/atelie-api/handler/product"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/product/:id", product.GetProductByIdHandler)
 		v1.GET("/products", product.GetAllProductsHendler)
@@ -17,4 +22,5 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/product/:id", product.UpdateProductHandler)
 		v1.DELETE("/product/:id",product.DeleteProductHandler)
 	}
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
